c949fc044bf0b38a4c6e1574840b07a067c12658
Elif Çiftçi <72947581+elifcifci@users.noreply.github.com>
2023-10-02 01:08:08 +0300

    chore:  project order was updated in projectConstant.js

--- src/Constants/projectConstant.js
+++ src/Constants/projectConstant.js
@@ -1,17 +1,17 @@
 export const projectConstant = [
   {
-    id: "0",
-    title: "My Personal Portfolio",
+    id: "3",
+    title: "Chuck Norris Facts",
     details: [
-      "This project was coded so that I could exhibit my projects.",
-      "Javascript, react, styled component, framer motion",
+      ` This website contains "Chuck Norris facts". Within the site, you can find jokes from different categories, temporarily add them to your basket or permanently save them to your saved page. You can create different users. Users' information is stored in localStorage.`,
+      " Javascript, react, CSS module, ant design",
     ],
-    imgLink: "./img/personal-portfolio.png",
-    previewLink: "",
-    codeLink: "https://github.com/elifcifci/elifcifci.github.io",
+    imgLink: "./img/joke-create.png",
+    previewLink: "https://joke-generator-psi.vercel.app/",
+    codeLink: "https://github.com/elifcifci/joke-generator",
     selected: false,
   },
-  {
+    {
     id: "1",
     title: "Rockstart Games Clone",
     details: [
@@ -21,6 +21,18 @@ export const projectConstant = [
     imgLink: "./img/rockstar-clone.jpg",
     previewLink: "https://rockstar-games-clone-website.netlify.app/",
     codeLink: "https://github.com/elifcifci/rockstar-games-clone",
+    selected: false,
+  },
+  {
+    id: "0",
+    title: "My Personal Portfolio",
+    details: [
+      "This project was coded so that I could exhibit my projects.",
+      "Javascript, react, styled component, framer motion",
+    ],
+    imgLink: "./img/personal-portfolio.png",
+    previewLink: "",
+    codeLink: "https://github.com/elifcifci/elifcifci.github.io",
     selected: false,
   },
   {
@@ -34,17 +46,5 @@ export const projectConstant = [
     previewLink: "https://elifciftci-portfolio-template.netlify.app/",
     codeLink: "https://elifciftci-portfolio-template.netlify.app/",
     selected: false,
-  },
-  {
-    id: "3",
-    title: "Chuck Norris Facts",
-    details: [
-      ` This website contains "Chuck Norris facts". Within the site, you can find jokes from different categories, temporarily add them to your basket or permanently save them to your saved page. You can create different users. Users' information is stored in localStorage.`,
-      " Javascript, react, CSS module, ant design",
-    ],
-    imgLink: "./img/joke-create.png",
-    previewLink: "https://joke-generator-psi.vercel.app/",
-    codeLink: "https://github.com/elifcifci/joke-generator",
-    selected: false,
-  },
+  }
 ];
