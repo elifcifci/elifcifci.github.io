0ed05b869ee1f69a93fbf69dca934d4049e2fbb2
Elif Çiftçi <eliffbat@gmail.com>
2024-04-16 23:19:49 +0300

    feat: project constant updated

--- src/Constants/projectConstant.js
+++ src/Constants/projectConstant.js
@@ -1,6 +1,18 @@
 export const projectConstant = [
   {
     id: "4",
+    title: "Click & Craf",
+    details: [
+      `In this project, the user experiences an animated landing page.`,
+      "Next.js 14, Typescript, Redux toolkit, Tailwind",
+    ],
+    imgLink: "./img/click-and-craft.webp",
+    previewLink: "https://click-and-craft.vercel.app/",
+    codeLink: "https://github.com/elifcifci/click-craft",
+    selected: false,
+  },
+  {
+    id: "3",
     title: "Evil Morty",
     details: [
       `This website contains web pages that you can visit by clicking on portals or by driving the spaceship with the keyboard.`,
@@ -12,7 +24,7 @@ export const projectConstant = [
     selected: false,
   },
   {
-    id: "3",
+    id: "2",
     title: "Chuck Norris Facts",
     details: [
       ` This website contains "Chuck Norris facts". Within the site, you can find jokes from different categories, temporarily add them to your basket or permanently save them to your saved page. You can create different users. Users' information is stored in localStorage.`,
@@ -34,29 +46,5 @@ export const projectConstant = [
     previewLink: "https://rockstar-games-clone-website.netlify.app/",
     codeLink: "https://github.com/elifcifci/rockstar-games-clone",
     selected: false,
-  },
-  {
-    id: "0",
-    title: "My Personal Portfolio",
-    details: [
-      "This project was coded so that I could exhibit my projects.",
-      "Javascript, react, styled component, framer motion",
-    ],
-    imgLink: "./img/personal-portfolio.png",
-    previewLink: "",
-    codeLink: "https://github.com/elifcifci/elifcifci.github.io",
-    selected: false,
-  },
-  {
-    id: "2",
-    title: "Portfolio Website Template",
-    details: [
-      "This project was coded so that the software developers could exhibit their projects.",
-      "Javascript, react, styled component, framer motion",
-    ],
-    imgLink: "./img/portfolio-template.png",
-    previewLink: "https://elifciftci-portfolio-template.netlify.app/",
-    codeLink: "https://elifciftci-portfolio-template.netlify.app/",
-    selected: false,
-  },
+  }
 ];
